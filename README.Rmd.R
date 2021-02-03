34bef0cf9e854fdc9abd136fb9fe492d1ffd9950
Shane DiGiovanna <shanespace1701@gmail.com>
2021-02-03 12:37:55 -0500

    Testing

--- README.Rmd.R
+++ README.Rmd.R
@@ -1,13 +1,14 @@
----
-title: "Roaring Twenties Elections in Ohio"
+# myrepotest2
+testing 123
+
+----
+  title: "Roaring Twenties Elections in Ohio"
 author: "Shane DiGiovanna"
 date: "January 30, 2021"
-output: 
-  github_document:
-    html_preview: false
+output: github_document
 ---
-
-```{r setup, include=FALSE}
+  
+  ```{r setup, include=FALSE}
 knitr::opts_chunk$set(echo = TRUE, warning = FALSE, message = FALSE)
 library(dplyr)
 library(ggplot2)
@@ -33,18 +34,18 @@ and I have done some preliminary processing to make the next steps easier. You c
 The county shapefile can be downloaded from [here](https://community.esri.com/t5/arcgis-enterprise-portal/where-can-i-find-a-shapefile-with-all-us-counties-and-fips-code/td-p/307592).
 
 We will read in the data:
-
-```{r data}
+  
+  ```{r data}
 twenties_data <- read_csv("~/Desktop/R Stuff/Ohio Coolidge Hoover elections.csv")
 
 county_shp <- st_read("/Users/shanedigiovanna/Downloads/UScounties/UScounties.shp")
 ```
 
 We need to filter `county_shp` to include only Ohio counties, and adjust the case of the county names in `twenties_data` to match those in `oh_county_shp`:
-
-```{r}
+  
+  ```{r}
 oh_county_shp <- county_shp %>%
-filter(STATE_NAME == "Ohio")
+  filter(STATE_NAME == "Ohio")
 
 twenties_data$COUNTY_NAME <- tolower(twenties_data$COUNTY_NAME)
 
@@ -55,8 +56,8 @@ twenties_data$COUNTY_NAME <- str_to_title(twenties_data$COUNTY_NAME)
 ## Merge the two dataframes
 
 Next, we need to merge the election data, `twenties_data`, with the county shapefile, `oh_county_shp`:
-
-```{r}
+  
+  ```{r}
 twenties_elect <- full_join(twenties_data, oh_county_shp, by = c("COUNTY_NAME" = "NAME"))
 
 #Make it into an sf
@@ -65,53 +66,53 @@ twenties_elect <- st_as_sf(twenties_elect)
 ##Create new columns
 
 twenties_elect <- twenties_elect %>%
-mutate(
-per_dem_pres_24 = (pres_democratic_vote_24 / pres_total_vote_24)*100,
-per_rep_pres_24 = (pres_republican_vote_24 / pres_total_vote_24)*100,
-per_prog_pres_24 = (pres_progressive_vote_24 / pres_total_vote_24)*100,
-per_dem_pres_28 = (pres_democratic_vote_28 / pres_total_vote_28)*100,
-per_rep_pres_28 = (pres_republican_vote_28 / pres_total_vote_28)*100,
-per_soc_pres_28 = (pres_socialist_vote_28 / pres_total_vote_28)*100,
-per_proh_pres_28 = (pres_prohibition_vote_28 / pres_total_vote_28)*100
-)
+  mutate(
+    per_dem_pres_24 = (pres_democratic_vote_24 / pres_total_vote_24)*100,
+    per_rep_pres_24 = (pres_republican_vote_24 / pres_total_vote_24)*100,
+    per_prog_pres_24 = (pres_progressive_vote_24 / pres_total_vote_24)*100,
+    per_dem_pres_28 = (pres_democratic_vote_28 / pres_total_vote_28)*100,
+    per_rep_pres_28 = (pres_republican_vote_28 / pres_total_vote_28)*100,
+    per_soc_pres_28 = (pres_socialist_vote_28 / pres_total_vote_28)*100,
+    per_proh_pres_28 = (pres_prohibition_vote_28 / pres_total_vote_28)*100
+  )
 
 twenties_elect <- twenties_elect %>%
-mutate(
-d_r_pres_margin_24 = per_dem_pres_24 - per_rep_pres_24,
-d_r_pres_margin_28 = per_dem_pres_28 - per_rep_pres_28
-)
+  mutate(
+    d_r_pres_margin_24 = per_dem_pres_24 - per_rep_pres_24,
+    d_r_pres_margin_28 = per_dem_pres_28 - per_rep_pres_28
+  )
 
 twenties_elect <- twenties_elect %>%
-mutate(
-margin_shift_24_28 = d_r_pres_margin_28 - d_r_pres_margin_24
-)
+  mutate(
+    margin_shift_24_28 = d_r_pres_margin_28 - d_r_pres_margin_24
+  )
 ```
 
 
 ## Mapping
 
 Now that we have processed and merged our data, we can map it! 
-
-First up is the 1924 election that returned Calvin Coolidge to the White House:
-```{r}
+  
+  First up is the 1924 election that returned Calvin Coolidge to the White House:
+  ```{r}
 oh_24_margin_map <- tm_shape(twenties_elect) + tm_polygons("d_r_pres_margin_24", palette = "RdBu", breaks = seq(-50, 50, by = 10),
-title = "1924 D/R Margin\n(in Percent)", id = "district", popup.vars = c("Dem/Rep Pres Margin" = "d_r_pres_margin_24", "Dem %" = "per_dem_pres_24", 
-    "Rep %" = "per_rep_pres_24")) + tm_view(alpha = 0.6, 
-legend.position = c("right", "bottom")) +
-    tm_layout(main.title = "Ohio 1924 Presidential\nParty Margin by County", main.title.position = "center", legend.title.size = 0.9, legend.outside = TRUE) +
-    tm_credits(text = "Coolidge won\nOhio by 34.63%\n\nShane DiGiovanna", position = c("left", "bottom"), size = 0.8)
+                                                           title = "1924 D/R Margin\n(in Percent)", id = "district", popup.vars = c("Dem/Rep Pres Margin" = "d_r_pres_margin_24", "Dem %" = "per_dem_pres_24", 
+                                                                                                                                    "Rep %" = "per_rep_pres_24")) + tm_view(alpha = 0.6, 
+                                                                                                                                                                            legend.position = c("right", "bottom")) +
+  tm_layout(main.title = "Ohio 1924 Presidential\nParty Margin by County", main.title.position = "center", legend.title.size = 0.9, legend.outside = TRUE) +
+  tm_credits(text = "Coolidge won\nOhio by 34.63%\n\nShane DiGiovanna", position = c("left", "bottom"), size = 0.8)
 
 oh_24_margin_map
 ```
 
 Republican dominance continued with Herbert Hoovers election in 1928, as shown in this map:
-```{r}
+  ```{r}
 oh_28_margin_map <- tm_shape(twenties_elect) + tm_polygons("d_r_pres_margin_28", palette = "RdBu", breaks = seq(-50, 50, by = 10),
-title = "1928 D/R Margin\n(in Percent)", id = "district", popup.vars = c("Dem/Rep Pres Margin" = "d_r_pres_margin_28", "Dem %" = "per_dem_pres_28", 
-    "Rep %" = "per_rep_pres_28")) + tm_view(alpha = 0.6, 
-legend.position = c("right", "bottom")) +
-    tm_layout(main.title = "Ohio 1928 Presidential Party Margin by County", main.title.position = "center", legend.title.size = 0.9, legend.outside = TRUE) +
-    tm_credits(text = "Hoover won\nOhio by 30.44%\n\nShane DiGiovanna", position = c("left", "bottom"), size = 0.8)
+                                                           title = "1928 D/R Margin\n(in Percent)", id = "district", popup.vars = c("Dem/Rep Pres Margin" = "d_r_pres_margin_28", "Dem %" = "per_dem_pres_28", 
+                                                                                                                                    "Rep %" = "per_rep_pres_28")) + tm_view(alpha = 0.6, 
+                                                                                                                                                                            legend.position = c("right", "bottom")) +
+  tm_layout(main.title = "Ohio 1928 Presidential Party Margin by County", main.title.position = "center", legend.title.size = 0.9, legend.outside = TRUE) +
+  tm_credits(text = "Hoover won\nOhio by 30.44%\n\nShane DiGiovanna", position = c("left", "bottom"), size = 0.8)
 
 oh_28_margin_map
 ```
@@ -126,10 +127,10 @@ La Follette performed well in Ohio, getting 17.75%. He did especially well in in
 
 ```{r}
 oh_24_prog_map <- tm_shape(twenties_elect) + tm_polygons("per_prog_pres_24", palette = "Greens", breaks = seq(0, 25, by = 2.5),
-title = "        Progressive\n  Vote Share\n(in Percent)", id = "district", popup.vars = c("Progressive %" = "per_prog_pres_24")) + tm_view(alpha = 0.6, 
-legend.position = c("right", "bottom")) +
-    tm_layout(main.title = "Ohio Robert La Follette Vote Share in\n1924 Presidential Election", main.title.position = "center", legend.title.size = 0.9, legend.outside = TRUE) +
-    tm_credits(text = "Progressive\nVote: 17.75%\n\nShane DiGiovanna", position = c("left", "bottom"), size = 0.8)
+                                                         title = "        Progressive\n  Vote Share\n(in Percent)", id = "district", popup.vars = c("Progressive %" = "per_prog_pres_24")) + tm_view(alpha = 0.6, 
+                                                                                                                                                                                                     legend.position = c("right", "bottom")) +
+  tm_layout(main.title = "Ohio Robert La Follette Vote Share in\n1924 Presidential Election", main.title.position = "center", legend.title.size = 0.9, legend.outside = TRUE) +
+  tm_credits(text = "Progressive\nVote: 17.75%\n\nShane DiGiovanna", position = c("left", "bottom"), size = 0.8)
 
 oh_24_prog_map
 
